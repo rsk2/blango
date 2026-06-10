9bea4b1e1e8ee2bc28e42acf058a67238cc86f84
Rohan Rohan <rohan.skumar@hotmail.com>
2026-06-10 09:49:30 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -138,6 +138,29 @@ class Dev(Configuration):
     CSRF_COOKIE_SAMESITE = 'None'
     SESSION_COOKIE_SAMESITE = 'None'
 
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "verbose": {
+                "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+                "style": "{",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "verbose",
+            },
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    }
+
+
 
 class Prod(Dev):
     DEBUG = False
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post
+import logging
+
+logger=logging.getLogger(__name__)
 
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
+    logger.debug(index.__name__)
     return render(request, "blog/index.html", {"posts": posts})
   
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
+    logger.info(
+      "Created comment on Post %d for user %s", post.pk, request.user
+    )
+
     return render(request, "blog/post-detail.html", {"post": post})
